27f6d652744dd5e2ceb1f9d16b44afadbad1b69c
rusafan <afanasyev.ruslan@gmail.com>
2025-06-08 01:59:38 +0300

    5. Tracker - хранилище [#396 #545484]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,20 +14,11 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] result = new Item[items.length];
-        int idx = 0;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item != null) {
-                result[idx] = item;
-                idx++;
-            }
-        }
-        return Arrays.copyOf(result, idx);
+        return Arrays.copyOf(items, size);
     }
 
     public Item[] findByName(String key) {
-        Item[] result = new Item[items.length];
+        Item[] result = new Item[size];
         int idx = 0;
         for (int index = 0; index < size; index++) {
             Item item = items[index];
@@ -49,29 +40,5 @@ public class Tracker {
             }
         }
         return rsl;
-    }
-
-    public static void main(String[] args) {
-        Tracker tracker = new Tracker();
-        Item item1 = new Item("заявка1");
-        //item1.setName("заявка1");
-        tracker.add(item1);
-        System.out.println("id = " + tracker.ids);
-        System.out.println("size = " + tracker.size);
-        Item item2 = new Item("заявка2");
-        //item2.setName("заявка2");
-        tracker.add(item2);
-        System.out.println("id = " + tracker.ids);
-        System.out.println("size = " + tracker.size);
-        System.out.println("Выводим заявки из массива заявок = " + tracker.items[0].getName());
-        System.out.println("Выводим заявки из массива заявок = " + tracker.items[1].getName());
-        //System.out.println("Выводим заявки из массива заявок = " + tracker.items[2].getName());
-        //System.out.println("Копия массива items без NULL элементов = " + res.);
-        Item[] res = tracker.findAll();
-        System.out.println(res.length);
-        System.out.println(res[0].getName());
-        System.out.println(res[1].getName());
-        Item res2 = tracker.findAll()[1];
-        System.out.println("Вывод res2 - " + res2.getName());
     }
 }
